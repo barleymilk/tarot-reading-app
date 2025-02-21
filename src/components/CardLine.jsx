36d38456dcecda62fbae7b983c208af3efbd4efd
barleymilk <barleymilk640@gmail.com>
2025-02-21 17:26:46 +0900

    feat: Add cardCount prop at CardLine Component

--- src/components/CardLine.jsx
+++ src/components/CardLine.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import styles from './CardLine.module.css';
 
-function CardLine({ cardNum = 0, startNum = 0, onCardClick, selectedCards }) {
+function CardLine({
+  cardNum = 0,
+  startNum = 0,
+  onCardClick,
+  selectedCards,
+  cardCount = 3,
+}) {
   const [clickedCard, setClickedCard] = useState(null);
 
   const handleClick = i => {
-    if (!selectedCards.includes(i) && selectedCards.length < 3) {
+    if (!selectedCards.includes(i) && selectedCards.length < cardCount) {
       setClickedCard(i);
       setTimeout(() => {
         onCardClick(i); // 애니메이션 후 카드 선택
